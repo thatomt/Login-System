7c84d2d3f65d8f9c67b325e3d4e4d0e0a1984b22
Thato M <thatomsekele@gmail.com>
2026-03-10 00:23:57 +0200

    Update login.py

--- login.py
+++ login.py
@@ -1,3 +1,7 @@
 def login(username, password):
       correct_username = "admin"
       correct_password = "1234"
+      
+      if username == correct_username and password == correct_password:
+        return True
+      return False
